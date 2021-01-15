8e666f3b71a42f744c9e1ff1a4267dc0cb3ece1b
Thao <thaobube@gmail.com>
2021-01-15 23:42:32 +0100

    first commit

---
+++ Netflax/App_Start/BundleConfig.cs
@@ -0,0 +1,55 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Netflax
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
+
+            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
+            //// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js"));
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
+
+
+            bundles.Add(new StyleBundle("~/Content/css").Include
+                (
+                    "~/Content/bootstrap.css",
+                    "~/css/style.css"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/JqueryFlexiSel").Include
+                (
+                    "~/js/jquery.flexisel.js"
+                ));
+
+            bundles.Add(new StyleBundle("~/Content/manificpopup").Include
+                (
+                    "~/css/popuo-box.css"
+                ));
+
+            bundles.Add(new StyleBundle("~/bundles/manificpopup").Include
+                (
+                    "~/js/jquery.magnific-popup.js"
+                ));
+        }
+    }
+}
---
+++ Netflax/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Netflax.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Home = "active";
+            return View();
+        }
+
+        public ActionResult Videos()
+        {
+            ViewBag.Videos = "active";
+            return View();
+        }
+
+        public ActionResult Reviews()
+        {
+            ViewBag.Reviews = "active";
+            return View();
+        }
+        public ActionResult Page404()
+        {
+            ViewBag.Page404 = "active";
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Contact = "active";
+            return View();
+        }
+    }
+}
